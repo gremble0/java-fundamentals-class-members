6e2127922b0a316cc6aecd3e2031c32cf3af0d60
gremble0 <hermanstornes@gmail.com>
2024-08-12 11:54:46 +0200

    Make tests pass for class members

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -1,40 +1,37 @@
 package com.booleanuk.core;
+
 import com.booleanuk.helpers.ExerciseBase;
 
 public class Exercise extends ExerciseBase {
     // Below is an example of a class member in Java.
     // Where a class describes a thing, a member describes its attributes.
-    // If Car were a class, some members of it might be colour, transmission, make and model.
-    // The structure of defining a member is <visibility> <data type> <name> = <value>
-    // The below member has a visibility of public, meaning other classes can see and change the value.
+    // If Car were a class, some members of it might be colour, transmission, make
+    // and model.
+    // The structure of defining a member is <visibility> <data type> <name> =
+    // <value>
+    // The below member has a visibility of public, meaning other classes can see
+    // and change the value.
     // It has data type of int, which stands for integer. A numeric value.
     // Its name can be anything as long as that name isn't already in use.
     public int answerToTheUltimateQuestion = 42;
 
-
     // 1. Create an integer member named age with a value of 32
-
-
+    public int age = 32;
 
     // 2. Create a String member named firstName with a value of "Jane"
-
-
+    public String firstName = "Jane";
 
     // 3. Create a boolean member named isProgrammer with a value of true
+    public boolean isProgrammer = true;
 
-
-
-    // 4. Change the value below so that the tests pass. Check the README.md file for instructions on
+    // 4. Change the value below so that the tests pass. Check the README.md file
+    // for instructions on
     // running and reading tests
-    public int firstNumber = 109;
-
-
+    public int firstNumber = 9182;
 
     // 5. Change the value below so that the tests pass
-    public String firstString = "Ruby is to Python what car is to carpet.";
-
-
+    public String firstString = "Java is to Javascript what car is to carpet.";
 
     // 6. Change the visibility below so that the tests pass
-    private boolean isVisible = true;
+    public boolean isVisible = true;
 }
